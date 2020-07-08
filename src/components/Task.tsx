8abaec9e02125d7b1b3b4e96793363fee9115ab3
Ashvini Kumar <ashvini@esthenos.com>
2020-07-08 10:48:55 +0530

    task and subtask formatting and some changes

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,67 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../config/axios.config';
-import Header from './Header'
-import SubTask from './SubTask'
-import {Redirect} from "react-router";
-
+import React, { useEffect, useState } from "react";
+import axios from "../config/axios.config";
+import Header from "./Header";
+import SubTask from "./SubTask";
+import { Redirect } from "react-router";
 
 function Task(props: any) {
   const [subTasks, setSubTasks] = useState([[]]);
-
   const id: number = props.match.params.id;
-  let token: string = localStorage.getItem("Token") || '';
-  console.log(`Token ${token}`)
+  let token: string = localStorage.getItem("Token") || "";
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchDetails = async () => {
-    if( token != ''){
-      console.log(`Token ${token}`)
+    if (token) {
       const res = await axios.get(`api/tasks/${id}`, {
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      })
+          Authorization: `Bearer ${token}`,
+        },
+      });
       const { data } = res;
       let allSubTasks = data.data.map((item: any) => {
-        console.log("Item", item.metaData);
         const task = {
           id: item.id,
           text: item.text,
           slug: item.slug,
           taskId: item.task_id,
           status: item.status,
-          tutorials: !item.metaData ? null : item.metaData.tutorials
-        }
-        console.log("Task", task);
+          tutorials: !item.metaData ? null : item.metaData.tutorials,
+        };
         return task;
       });
       setSubTasks(allSubTasks);
     }
-
   };
+
   useEffect(() => {
-    fetchDetails()
+    fetchDetails();
   }, []);
-  if(token == ''){
-    return <Redirect to={"/login"}/>;
+
+  if (!token) {
+    return <Redirect to={"/login"} />;
   }
-  console.log("All subtasks", subTasks);
-  return (
+
+  return subTasks.length > 0 ? (
     <>
       <Header />
-      {subTasks.map((task: any) => {
+      {subTasks.map((task: any, index) => {
         return (
-          <>
-            <SubTask
-              task={task}
-              updateAllTasks={ () => fetchDetails()}
-            />
-          </>
-        )
+          <SubTask
+            key={index}
+            task={task}
+            updateAllTasks={() => fetchDetails()}
+          />
+        );
       })}
-
     </>
+  ) : (
+    <h4>No Task at the moment!</h4>
   );
-
 }
 
 export default Task;
